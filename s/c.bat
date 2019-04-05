8e42b6ec0f684321803a38138c056a1fd1a18a16
handywebprojects <handywebprojects@gmail.com>
2019-04-05 13:23:22 +0200

    Store analysis when making move

--- s/c.bat
+++ s/c.bat
@@ -5,11 +5,13 @@ call s\ver
 git add .
 
 git reset -- static/clientjs/*
+git reset -- static/js/app.js
 git reset -- schemaconfig.json
 
 git commit -m "%*"
 
 git add -- static/clientjs/*
+git add -- static/js/app.js
 git add -- schemaconfig.json
 
 git commit -m "%* bin"
