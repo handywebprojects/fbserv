c4ad48fc680b00f03cea45c6cef9322e96154eb8
handywebprojects <handywebprojects@gmail.com>
2019-04-06 10:48:41 +0200

    Control button ordering in board bin

--- static/clientjs/board.js
+++ static/clientjs/board.js
@@ -1,4 +1,4 @@
-// Transcrypt'ed from Python, 2019-04-06 10:20:41
+// Transcrypt'ed from Python, 2019-04-06 10:46:39
 import {AssertionError, AttributeError, BaseException, DeprecationWarning, Exception, IndexError, IterableError, KeyError, NotImplementedError, RuntimeWarning, StopIteration, UserWarning, ValueError, Warning, __JsIterator__, __PyIterator__, __Terminal__, __add__, __and__, __call__, __class__, __envir__, __eq__, __floordiv__, __ge__, __get__, __getcm__, __getitem__, __getslice__, __getsm__, __gt__, __i__, __iadd__, __iand__, __idiv__, __ijsmod__, __ilshift__, __imatmul__, __imod__, __imul__, __in__, __init__, __ior__, __ipow__, __irshift__, __isub__, __ixor__, __jsUsePyNext__, __jsmod__, __k__, __kwargtrans__, __le__, __lshift__, __lt__, __matmul__, __mergefields__, __mergekwargtrans__, __mod__, __mul__, __ne__, __neg__, __nest__, __or__, __pow__, __pragma__, __proxy__, __pyUseJsNext__, __rshift__, __setitem__, __setproperty__, __setslice__, __sort__, __specialattrib__, __sub__, __super__, __t__, __terminal__, __truediv__, __withblock__, __xor__, abs, all, any, assert, bool, bytearray, bytes, callable, chr, copy, deepcopy, delattr, dict, dir, divmod, enumerate, filter, float, getattr, hasattr, input, int, isinstance, issubclass, len, list, map, max, min, object, ord, pow, print, property, py_TypeError, py_iter, py_metatype, py_next, py_reversed, py_typeof, range, repr, round, set, setattr, sorted, str, sum, tuple, zip} from './org.transcrypt.__runtime__.js';
 import {LICH_API_GAMES_EXPORT, getconn, lichapiget} from './connection.js';
 import {Log, LogItem} from './widgets.js';
@@ -598,12 +598,18 @@ export var Board =  __class__ ('Board', [e], {
 	});},
 	get getgamesan () {return __get__ (this, function (self) {
 		try {
-			var pinfo = self.positioninfos [self.gamei + 1] ['positioninfo'];
+			var pinfoitem = self.positioninfos [self.gamei];
+			var pinfoitemnext = self.positioninfos [self.gamei + 1];
+			var fen = pinfoitem ['fen'];
+			var pinfo = pinfoitemnext ['positioninfo'];
+			if (!(fen == self.basicboard.fen)) {
+				return null;
+			}
 			var san = pinfo ['genmove'] ['san'];
 			return san;
 		}
 		catch (__except0__) {
-			// pass;
+			return null;
 		}
 	});},
 	get buildanalysisinfodiv () {return __get__ (this, function (self) {
@@ -876,10 +882,10 @@ export var Board =  __class__ ('Board', [e], {
 		self.variantcombo = ComboBox ();
 		self.setvariantcombo ();
 		self.controlpanel.a (self.variantcombo).w (self.basicboard.outerwidth).mw (self.basicboard.outerwidth);
-		self.controlpanel.a (Button ('Del', self.delcallback));
-		self.controlpanel.a (Button ('Delall', self.delallcallback));
-		self.controlpanel.a (Button ('Clearall', self.clearcallback));
 		self.controlpanel.a (Button ('Reset', self.setvariantcallback));
+		self.controlpanel.a (Button ('Clearall', self.clearcallback));
+		self.controlpanel.a (Button ('Delall', self.delallcallback));
+		self.controlpanel.a (Button ('Del', self.delcallback));
 		self.sectioncontainer = Div ().ac ('bigboardsectioncontainer').w (self.basicboard.outerwidth);
 		self.sectioncontainer.bci (self.background);
 		self.sectioncontainer.a ([self.controlpanel, self.basicboard]);
