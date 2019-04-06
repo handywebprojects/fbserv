bbabfb3a9b437de656815ea8febaff19006004be
handywebprojects <handywebprojects@gmail.com>
2019-04-06 10:22:25 +0200

    Show game move in analysis bin

--- static/clientjs/basicboard.js
+++ static/clientjs/basicboard.js
@@ -1,4 +1,4 @@
-// Transcrypt'ed from Python, 2019-04-05 13:17:44
+// Transcrypt'ed from Python, 2019-04-06 10:12:39
 import {AssertionError, AttributeError, BaseException, DeprecationWarning, Exception, IndexError, IterableError, KeyError, NotImplementedError, RuntimeWarning, StopIteration, UserWarning, ValueError, Warning, __JsIterator__, __PyIterator__, __Terminal__, __add__, __and__, __call__, __class__, __envir__, __eq__, __floordiv__, __ge__, __get__, __getcm__, __getitem__, __getslice__, __getsm__, __gt__, __i__, __iadd__, __iand__, __idiv__, __ijsmod__, __ilshift__, __imatmul__, __imod__, __imul__, __in__, __init__, __ior__, __ipow__, __irshift__, __isub__, __ixor__, __jsUsePyNext__, __jsmod__, __k__, __kwargtrans__, __le__, __lshift__, __lt__, __matmul__, __mergefields__, __mergekwargtrans__, __mod__, __mul__, __ne__, __neg__, __nest__, __or__, __pow__, __pragma__, __proxy__, __pyUseJsNext__, __rshift__, __setitem__, __setproperty__, __setslice__, __sort__, __specialattrib__, __sub__, __super__, __t__, __terminal__, __truediv__, __withblock__, __xor__, abs, all, any, assert, bool, bytearray, bytes, callable, chr, copy, deepcopy, delattr, dict, dir, divmod, enumerate, filter, float, getattr, hasattr, input, int, isinstance, issubclass, len, list, map, max, min, object, ord, pow, print, property, py_TypeError, py_iter, py_metatype, py_next, py_reversed, py_typeof, range, repr, round, set, setattr, sorted, str, sum, tuple, zip} from './org.transcrypt.__runtime__.js';
 import {getconn} from './connection.js';
 import {Vect, cpick, scorecolor, scoreverbal, xor} from './utils.js';
@@ -794,7 +794,10 @@ export var MultipvInfo =  __class__ ('MultipvInfo', [e], {
 			self.bonussliderchangedcallback ();
 		}
 	});},
-	get build () {return __get__ (this, function (self) {
+	get build () {return __get__ (this, function (self, gamesan) {
+		if (typeof gamesan == 'undefined' || (gamesan != null && gamesan.hasOwnProperty ("__kwargtrans__"))) {;
+			var gamesan = null;
+		};
 		self.bestmoveuci = self.infoi ['bestmoveuci'];
 		self.bestmovesan = self.infoi ['bestmovesan'];
 		self.scorenumerical = self.infoi ['scorenumerical'];
@@ -805,6 +808,9 @@ export var MultipvInfo =  __class__ ('MultipvInfo', [e], {
 		self.container = Div ().ac ('multipvinfocontainer');
 		self.idiv = Div ().ac ('multipvinfoi').html ('{}.'.format (self.i));
 		self.bestmovesandiv = Div ().ac ('multipvinfobestmovesan').html (self.bestmovesan);
+		if (gamesan == self.bestmovesan) {
+			self.bestmovesandiv.bc ('#fbf');
+		}
 		self.bestmovesandiv.ae ('mousedown', self.bestmovesanclickedfactory (self.bestmoveuci));
 		self.scorenumericaldiv = Div ().ac ('multipvinfoscorenumerical').html ('{}'.format (scoreverbal (self.effscore ()))).cp ();
 		self.scorenumericaldiv.ae ('mousedown', self.bestmovesanclickedfactory (self.bestmoveuci, true));
