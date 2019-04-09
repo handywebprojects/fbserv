08fd3d55ed46ad4f3a4a6c5e2eb5512b26756d78
handywebprojects <handywebprojects@gmail.com>
2019-04-09 16:42:51 +0200

    Switch to analysis when game position clicked bin

--- static/clientjs/board.js
+++ static/clientjs/board.js
@@ -1,4 +1,4 @@
-// Transcrypt'ed from Python, 2019-04-09 07:51:16
+// Transcrypt'ed from Python, 2019-04-09 16:40:55
 import {AssertionError, AttributeError, BaseException, DeprecationWarning, Exception, IndexError, IterableError, KeyError, NotImplementedError, RuntimeWarning, StopIteration, UserWarning, ValueError, Warning, __JsIterator__, __PyIterator__, __Terminal__, __add__, __and__, __call__, __class__, __envir__, __eq__, __floordiv__, __ge__, __get__, __getcm__, __getitem__, __getslice__, __getsm__, __gt__, __i__, __iadd__, __iand__, __idiv__, __ijsmod__, __ilshift__, __imatmul__, __imod__, __imul__, __in__, __init__, __ior__, __ipow__, __irshift__, __isub__, __ixor__, __jsUsePyNext__, __jsmod__, __k__, __kwargtrans__, __le__, __lshift__, __lt__, __matmul__, __mergefields__, __mergekwargtrans__, __mod__, __mul__, __ne__, __neg__, __nest__, __or__, __pow__, __pragma__, __proxy__, __pyUseJsNext__, __rshift__, __setitem__, __setproperty__, __setslice__, __sort__, __specialattrib__, __sub__, __super__, __t__, __terminal__, __truediv__, __withblock__, __xor__, abs, all, any, assert, bool, bytearray, bytes, callable, chr, copy, deepcopy, delattr, dict, dir, divmod, enumerate, filter, float, getattr, hasattr, input, int, isinstance, issubclass, len, list, map, max, min, object, ord, pow, print, property, py_TypeError, py_iter, py_metatype, py_next, py_reversed, py_typeof, range, repr, round, set, setattr, sorted, str, sum, tuple, zip} from './org.transcrypt.__runtime__.js';
 import {LICH_API_GAMES_EXPORT, getconn, lichapiget} from './connection.js';
 import {Log, LogItem} from './widgets.js';
@@ -71,18 +71,27 @@ export var Board =  __class__ ('Board', [e], {
 	get setvariantcombo () {return __get__ (this, function (self) {
 		self.variantcombo.setoptions (VARIANT_OPTIONS, self.basicboard.variantkey, self.variantchanged);
 	});},
-	get posclickedfactory () {return __get__ (this, function (self, i) {
-		var poslicked = function () {
-			self.gamei = i;
-			var pinfo = self.positioninfos [i];
-			self.setfromfen (pinfo ['fen'], pinfo ['positioninfo']);
+	get showcurrentgamepos () {return __get__ (this, function (self) {
+		try {
 			for (var j = 0; j < len (self.positioninfos); j++) {
 				self.posdivs [j].arc (j == self.gamei, 'boardposdivselected');
 				if (j == self.gamei) {
 					self.posdivs [j].e.scrollIntoView (dict ({'block': 'center', 'inline': 'center'}));
 				}
 			}
+		}
+		catch (__except0__) {
+			// pass;
+		}
+	});},
+	get posclickedfactory () {return __get__ (this, function (self, i) {
+		var poslicked = function () {
+			self.gamei = i;
+			var pinfo = self.positioninfos [i];
+			self.setfromfen (pinfo ['fen'], pinfo ['positioninfo']);
+			self.showcurrentgamepos ();
 			self.history = [];
+			self.tabpane.selectbykey ('analysis');
 		};
 		return poslicked;
 	});},
@@ -845,12 +854,7 @@ export var Board =  __class__ ('Board', [e], {
 		self.variantchanged (self.basicboard.variantkey, fen);
 	});},
 	get gametabselected () {return __get__ (this, function (self) {
-		try {
-			self.posclickedfactory (self.gamei) ();
-		}
-		catch (__except0__) {
-			// pass;
-		}
+		self.showcurrentgamepos ();
 	});},
 	get trainhandler () {return __get__ (this, function (self) {
 		try {
