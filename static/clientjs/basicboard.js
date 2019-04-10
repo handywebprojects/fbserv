de07c871f4238ee26e2316db4c5c924175547913
handywebprojects <handywebprojects@gmail.com>
2019-04-10 12:38:33 +0200

    Jump to bookmarked game bin

--- static/clientjs/basicboard.js
+++ static/clientjs/basicboard.js
@@ -1,4 +1,4 @@
-// Transcrypt'ed from Python, 2019-04-09 10:43:50
+// Transcrypt'ed from Python, 2019-04-10 12:35:08
 import {AssertionError, AttributeError, BaseException, DeprecationWarning, Exception, IndexError, IterableError, KeyError, NotImplementedError, RuntimeWarning, StopIteration, UserWarning, ValueError, Warning, __JsIterator__, __PyIterator__, __Terminal__, __add__, __and__, __call__, __class__, __envir__, __eq__, __floordiv__, __ge__, __get__, __getcm__, __getitem__, __getslice__, __getsm__, __gt__, __i__, __iadd__, __iand__, __idiv__, __ijsmod__, __ilshift__, __imatmul__, __imod__, __imul__, __in__, __init__, __ior__, __ipow__, __irshift__, __isub__, __ixor__, __jsUsePyNext__, __jsmod__, __k__, __kwargtrans__, __le__, __lshift__, __lt__, __matmul__, __mergefields__, __mergekwargtrans__, __mod__, __mul__, __ne__, __neg__, __nest__, __or__, __pow__, __pragma__, __proxy__, __pyUseJsNext__, __rshift__, __setitem__, __setproperty__, __setslice__, __sort__, __specialattrib__, __sub__, __super__, __t__, __terminal__, __truediv__, __withblock__, __xor__, abs, all, any, assert, bool, bytearray, bytes, callable, chr, copy, deepcopy, delattr, dict, dir, divmod, enumerate, filter, float, getattr, hasattr, input, int, isinstance, issubclass, len, list, map, max, min, object, ord, pow, print, property, py_TypeError, py_iter, py_metatype, py_next, py_reversed, py_typeof, range, repr, round, set, setattr, sorted, str, sum, tuple, zip} from './org.transcrypt.__runtime__.js';
 import {getconn} from './connection.js';
 import {Vect, cpick, scorecolor, scoreverbal, xor} from './utils.js';
@@ -881,10 +881,11 @@ export var MultipvInfo =  __class__ ('MultipvInfo', [e], {
 });
 export var PgnInfo =  __class__ ('PgnInfo', [e], {
 	__module__: __name__,
-	get __init__ () {return __get__ (this, function (self, parent) {
+	get __init__ () {return __get__ (this, function (self, parent, pgnlistparent) {
 		__super__ (PgnInfo, '__init__') (self, 'div');
 		self.headers = [];
 		self.parent = parent;
+		self.pgnlistparent = pgnlistparent;
 	});},
 	get getheader () {return __get__ (this, function (self, key, py_default) {
 		for (var header of self.headers) {
@@ -925,6 +926,7 @@ export var PgnInfo =  __class__ ('PgnInfo', [e], {
 		self.parent.pgntext.setpgn (self.content);
 		self.bds ('dotted').bdw ('6').bdc ('#00f');
 		localStorage.setItem ('pgninfo/idclicked', self.id);
+		self.pgnlistparent.bookmarkedpi = self;
 		getconn ().sioreq (dict ({'kind': 'parsepgn', 'owner': self.parent.id, 'data': self.content}));
 	});},
 	get mecolor () {return __get__ (this, function (self) {
@@ -1080,12 +1082,21 @@ export var PgnList =  __class__ ('PgnList', [e], {
 		__super__ (PgnList, '__init__') (self, 'div');
 		self.parent = parent;
 	});},
+	get tobookmark () {return __get__ (this, function (self) {
+		if (self.bookmarkedpi) {
+			self.bookmarkedpi.e.scrollIntoView (dict ({'block': 'center', 'inline': 'center'}));
+		}
+	});},
 	get build () {return __get__ (this, function (self) {
 		self.x ();
 		var first = true;
 		var dateinfos = [];
+		self.bookmarkedpi = null;
 		for (var gamecontent of self.gamecontents) {
-			var pi = PgnInfo (self.parent).setcontent (gamecontent);
+			var pi = PgnInfo (self.parent, self).setcontent (gamecontent);
+			if (pi.id == localStorage.getItem ('pgninfo/idclicked')) {
+				self.bookmarkedpi = pi;
+			}
 			self.a (pi);
 			var mri = pi.meratinginfo ();
 			if (mri) {
