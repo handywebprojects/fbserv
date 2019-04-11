db67e7519c0951343bceb57202d053b7292fb377
handywebprojects <handywebprojects@gmail.com>
2019-04-11 19:22:32 +0200

    Make the best move in most cases bin

--- static/clientjs/board.js
+++ static/clientjs/board.js
@@ -1,4 +1,4 @@
-// Transcrypt'ed from Python, 2019-04-11 18:34:56
+// Transcrypt'ed from Python, 2019-04-11 19:10:58
 import {AssertionError, AttributeError, BaseException, DeprecationWarning, Exception, IndexError, IterableError, KeyError, NotImplementedError, RuntimeWarning, StopIteration, UserWarning, ValueError, Warning, __JsIterator__, __PyIterator__, __Terminal__, __add__, __and__, __call__, __class__, __envir__, __eq__, __floordiv__, __ge__, __get__, __getcm__, __getitem__, __getslice__, __getsm__, __gt__, __i__, __iadd__, __iand__, __idiv__, __ijsmod__, __ilshift__, __imatmul__, __imod__, __imul__, __in__, __init__, __ior__, __ipow__, __irshift__, __isub__, __ixor__, __jsUsePyNext__, __jsmod__, __k__, __kwargtrans__, __le__, __lshift__, __lt__, __matmul__, __mergefields__, __mergekwargtrans__, __mod__, __mul__, __ne__, __neg__, __nest__, __or__, __pow__, __pragma__, __proxy__, __pyUseJsNext__, __rshift__, __setitem__, __setproperty__, __setslice__, __sort__, __specialattrib__, __sub__, __super__, __t__, __terminal__, __truediv__, __withblock__, __xor__, abs, all, any, assert, bool, bytearray, bytes, callable, chr, copy, deepcopy, delattr, dict, dir, divmod, enumerate, filter, float, getattr, hasattr, input, int, isinstance, issubclass, len, list, map, max, min, object, ord, pow, print, property, py_TypeError, py_iter, py_metatype, py_next, py_reversed, py_typeof, range, repr, round, set, setattr, sorted, str, sum, tuple, zip} from './org.transcrypt.__runtime__.js';
 import {LICH_API_GAMES_EXPORT, getconn, lichapiget} from './connection.js';
 import {Log, LogItem} from './widgets.js';
@@ -1047,6 +1047,9 @@ export var Board =  __class__ ('Board', [e], {
 								if (selsize > len (pvitems)) {
 									var selsize = len (pvitems);
 								}
+								if (int (Math.random () * 100) > 20) {
+									var selsize = 1;
+								}
 								var index = int (Math.random () * selsize);
 								if (index >= len (pvitems)) {
 									var index = len (pvitems) - 1;
@@ -1055,6 +1058,10 @@ export var Board =  __class__ ('Board', [e], {
 								self.trainfen = self.basicboard.fen;
 								self.moveclickedcallback (self.basicboard.variantkey, self.basicboard.fen, selectedalgeb, false);
 								self.showtraininfomsg ('Making random engine move [ {} from {} : {} ].'.format (index + 1, selsize, selectedalgeb));
+							}
+							else {
+								self.trainfen = self.basicboard.fen;
+								self.showtraininfomsg ('No legal moves.', 'err');
 							}
 						}
 					}
