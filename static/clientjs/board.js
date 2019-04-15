db44301a88c320676dca5819e368c07ea6fad50d
handywebprojects <handywebprojects@gmail.com>
2019-04-15 04:53:16 +0200

    Edit favorite line bin

--- static/clientjs/board.js
+++ static/clientjs/board.js
@@ -1,4 +1,4 @@
-// Transcrypt'ed from Python, 2019-04-13 07:37:56
+// Transcrypt'ed from Python, 2019-04-15 04:49:49
 import {AssertionError, AttributeError, BaseException, DeprecationWarning, Exception, IndexError, IterableError, KeyError, NotImplementedError, RuntimeWarning, StopIteration, UserWarning, ValueError, Warning, __JsIterator__, __PyIterator__, __Terminal__, __add__, __and__, __call__, __class__, __envir__, __eq__, __floordiv__, __ge__, __get__, __getcm__, __getitem__, __getslice__, __getsm__, __gt__, __i__, __iadd__, __iand__, __idiv__, __ijsmod__, __ilshift__, __imatmul__, __imod__, __imul__, __in__, __init__, __ior__, __ipow__, __irshift__, __isub__, __ixor__, __jsUsePyNext__, __jsmod__, __k__, __kwargtrans__, __le__, __lshift__, __lt__, __matmul__, __mergefields__, __mergekwargtrans__, __mod__, __mul__, __ne__, __neg__, __nest__, __or__, __pow__, __pragma__, __proxy__, __pyUseJsNext__, __rshift__, __setitem__, __setproperty__, __setslice__, __sort__, __specialattrib__, __sub__, __super__, __t__, __terminal__, __truediv__, __withblock__, __xor__, abs, all, any, assert, bool, bytearray, bytes, callable, chr, copy, deepcopy, delattr, dict, dir, divmod, enumerate, filter, float, getattr, hasattr, input, int, isinstance, issubclass, len, list, map, max, min, object, ord, pow, print, property, py_TypeError, py_iter, py_metatype, py_next, py_reversed, py_typeof, range, repr, round, set, setattr, sorted, str, sum, tuple, zip} from './org.transcrypt.__runtime__.js';
 import {LICH_API_GAMES_EXPORT, getconn, lichapiget} from './connection.js';
 import {Log, LogItem} from './widgets.js';
@@ -1053,12 +1053,19 @@ export var Board =  __class__ ('Board', [e], {
 	get storefavlines () {return __get__ (this, function (self, favlines) {
 		localStorage.setItem (self.id + '/favlines', JSON.stringify (favlines));
 	});},
-	get removefavlinefactory () {return __get__ (this, function (self, favline) {
+	get removefavlinefactory () {return __get__ (this, function (self, favline, edit) {
+		if (typeof edit == 'undefined' || (edit != null && edit.hasOwnProperty ("__kwargtrans__"))) {;
+			var edit = false;
+		};
 		var removefavline = function () {
 			var favlines = self.getfavlines ();
 			var newfavlines = [];
 			for (var fl of favlines) {
 				if (!(fl ['line'] == favline ['line'])) {
+					newfavlines.append (fl);
+				}
+				else if (edit) {
+					fl ['fen'] = self.basicboard.fen;
 					newfavlines.append (fl);
 				}
 			}
@@ -1075,7 +1082,7 @@ export var Board =  __class__ ('Board', [e], {
 			for (var favline of favlines) {
 				var favlinediv = Div ().disp ('flex').pad (1).mar (1).cp ();
 				var line = favline ['line'];
-				favlinediv.a ([Button ('-', self.removefavlinefactory (favline)).w (14).h (14).fs (7).bc ('#faa'), Div ().ml (3).html (line).ae ('mousedown', self.favlineclickedfactory (favline))]);
+				favlinediv.a ([Button ('*', self.removefavlinefactory (favline, true)).w (28).h (14).fs (7).bc ('#afa'), Button ('-', self.removefavlinefactory (favline)).w (14).h (14).fs (7).bc ('#faa').ml (2), Div ().ml (3).html (line).ae ('mousedown', self.favlineclickedfactory (favline))]);
 				self.favlinesdiv.a (favlinediv);
 			}
 			self.trainmode = self.traincombo.v ();
