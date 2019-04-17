bc10d04c764aef370352953798302431e47e16f1
handywebprojects <handywebprojects@gmail.com>
2019-04-17 15:18:52 +0200

    Highlight opacity by weight bin

--- static/clientjs/basicboard.js
+++ static/clientjs/basicboard.js
@@ -1,4 +1,4 @@
-// Transcrypt'ed from Python, 2019-04-17 12:24:59
+// Transcrypt'ed from Python, 2019-04-17 15:10:44
 import {AssertionError, AttributeError, BaseException, DeprecationWarning, Exception, IndexError, IterableError, KeyError, NotImplementedError, RuntimeWarning, StopIteration, UserWarning, ValueError, Warning, __JsIterator__, __PyIterator__, __Terminal__, __add__, __and__, __call__, __class__, __envir__, __eq__, __floordiv__, __ge__, __get__, __getcm__, __getitem__, __getslice__, __getsm__, __gt__, __i__, __iadd__, __iand__, __idiv__, __ijsmod__, __ilshift__, __imatmul__, __imod__, __imul__, __in__, __init__, __ior__, __ipow__, __irshift__, __isub__, __ixor__, __jsUsePyNext__, __jsmod__, __k__, __kwargtrans__, __le__, __lshift__, __lt__, __matmul__, __mergefields__, __mergekwargtrans__, __mod__, __mul__, __ne__, __neg__, __nest__, __or__, __pow__, __pragma__, __proxy__, __pyUseJsNext__, __rshift__, __setitem__, __setproperty__, __setslice__, __sort__, __specialattrib__, __sub__, __super__, __t__, __terminal__, __truediv__, __withblock__, __xor__, abs, all, any, assert, bool, bytearray, bytes, callable, chr, copy, deepcopy, delattr, dict, dir, divmod, enumerate, filter, float, getattr, hasattr, input, int, isinstance, issubclass, len, list, map, max, min, object, ord, pow, print, property, py_TypeError, py_iter, py_metatype, py_next, py_reversed, py_typeof, range, repr, round, set, setattr, sorted, str, sum, tuple, zip} from './org.transcrypt.__runtime__.js';
 import {getconn} from './connection.js';
 import {Vect, cpick, scorecolor, scoreverbal, xor} from './utils.js';
@@ -386,7 +386,7 @@ export var BasicBoard =  __class__ ('BasicBoard', [e], {
 			var sqdiv = Div ().ac (['boardsquare', sqclass]).w (self.squaresize).h (self.squaresize);
 			var sqhdiv = Div ().pa ().w (self.squaresize).h (self.squaresize);
 			self.sqdivs [sq.hashkey ()] = sqdiv;
-			self.sqhdivs [sq.hashkey ()] = sqhdiv;
+			self.sqhdivs [sq.hashkey ()] = dict ({'div': sqhdiv, 'cumop': 0.0});
 			var fasq = self.flipawaresquare (sq);
 			sqdiv.pv (self.squarecoordsvect (fasq));
 			sqhdiv.pv (self.squarecoordsvect (fasq));
@@ -499,19 +499,24 @@ export var BasicBoard =  __class__ ('BasicBoard', [e], {
 		};
 		self.drawmovearrow (self.ucitomove (uci), args);
 	});},
-	get highlightsquare () {return __get__ (this, function (self, sq, bc) {
+	get highlightsquare () {return __get__ (this, function (self, sq, bc, op) {
+		if (op == 0) {
+			return ;
+		}
 		var margin = self.squaresize * 0.1;
 		var hsize = self.squaresize - 2 * margin;
 		var sqhdiv = self.sqhdivs [sq.hashkey ()];
-		sqhdiv.a (Div ().pa ().t (margin).l (margin).w (hsize).h (hsize).bc (bc));
-		sqhdiv.op (0.75);
+		if (op > sqhdiv ['cumop']) {
+			sqhdiv ['cumop'] = op;
+		}
+		sqhdiv ['div'].x ().a (Div ().pa ().t (margin).l (margin).w (hsize).h (hsize).bc (bc).op (sqhdiv ['cumop']));
 	});},
-	get highlightmove () {return __get__ (this, function (self, move, bc) {
-		self.highlightsquare (move.fromsq, bc);
-		self.highlightsquare (move.tosq, bc);
+	get highlightmove () {return __get__ (this, function (self, move, bc, op) {
+		self.highlightsquare (move.fromsq, bc, op);
+		self.highlightsquare (move.tosq, bc, op);
 	});},
-	get highlightucimove () {return __get__ (this, function (self, uci, bc) {
-		self.highlightmove (self.ucitomove (uci), bc);
+	get highlightucimove () {return __get__ (this, function (self, uci, bc, op) {
+		self.highlightmove (self.ucitomove (uci), bc, op);
 	});},
 	get buildgenmove () {return __get__ (this, function (self) {
 		if (__in__ ('genmove', self.positioninfo)) {
@@ -894,6 +899,10 @@ export var MultipvInfo =  __class__ ('MultipvInfo', [e], {
 		else {
 			self.trainbc = 'inherit';
 		}
+		self.metrainweight = int (metrainweight);
+		self.opptrainweight = int (opptrainweight);
+		self.avgtrainweight = (self.metrainweight + self.opptrainweight) / 2.0;
+		self.trainop = self.avgtrainweight / 10.0;
 		self.metraincombo = ComboBox ().setoptions (TRAIN_OPTIONS, metrainweight, self.traincombochanged);
 		self.opptraincombo = ComboBox ().setoptions (TRAIN_OPTIONS, opptrainweight, self.traincombochanged);
 		self.traindiv.a ([self.metraincombo, self.opptraincombo]);
