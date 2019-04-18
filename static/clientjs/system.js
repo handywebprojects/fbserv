fc2feba9050deff1e8ac70b84b28a4c809941d44
handywebprojects <handywebprojects@gmail.com>
2019-04-18 09:56:41 +0200

    Serialize localstorage bin

--- static/clientjs/system.js
+++ static/clientjs/system.js
@@ -1,4 +1,4 @@
-// Transcrypt'ed from Python, 2019-04-05 10:13:03
+// Transcrypt'ed from Python, 2019-04-18 09:55:10
 import {AssertionError, AttributeError, BaseException, DeprecationWarning, Exception, IndexError, IterableError, KeyError, NotImplementedError, RuntimeWarning, StopIteration, UserWarning, ValueError, Warning, __JsIterator__, __PyIterator__, __Terminal__, __add__, __and__, __call__, __class__, __envir__, __eq__, __floordiv__, __ge__, __get__, __getcm__, __getitem__, __getslice__, __getsm__, __gt__, __i__, __iadd__, __iand__, __idiv__, __ijsmod__, __ilshift__, __imatmul__, __imod__, __imul__, __in__, __init__, __ior__, __ipow__, __irshift__, __isub__, __ixor__, __jsUsePyNext__, __jsmod__, __k__, __kwargtrans__, __le__, __lshift__, __lt__, __matmul__, __mergefields__, __mergekwargtrans__, __mod__, __mul__, __ne__, __neg__, __nest__, __or__, __pow__, __pragma__, __proxy__, __pyUseJsNext__, __rshift__, __setitem__, __setproperty__, __setslice__, __sort__, __specialattrib__, __sub__, __super__, __t__, __terminal__, __truediv__, __withblock__, __xor__, abs, all, any, assert, bool, bytearray, bytes, callable, chr, copy, deepcopy, delattr, dict, dir, divmod, enumerate, filter, float, getattr, hasattr, input, int, isinstance, issubclass, len, list, map, max, min, object, ord, pow, print, property, py_TypeError, py_iter, py_metatype, py_next, py_reversed, py_typeof, range, repr, round, set, setattr, sorted, str, sum, tuple, zip} from './org.transcrypt.__runtime__.js';
 import {Schema} from './schema.js';
 import {getconn} from './connection.js';
@@ -549,13 +549,40 @@ export var Config =  __class__ ('Config', [e], {
 	get serializeconfig () {return __get__ (this, function (self) {
 		getconn ().sioreq (dict ({'kind': 'serializeconfig', 'data': self.configschema.toargs (), 'owner': self.id}));
 	});},
+	get serializelocalstorage () {return __get__ (this, function (self) {
+		getconn ().sioreq (dict ({'kind': 'serializelocalstorage', 'data': JSON.stringify (localStorage), 'owner': self.id}));
+	});},
+	get synclocalstorage () {return __get__ (this, function (self) {
+		getconn ().sioreq (dict ({'kind': 'synclocalstorage', 'owner': self.id}));
+	});},
+	get siores () {return __get__ (this, function (self, response) {
+		try {
+			var kind = response ['kind'];
+			if (kind == 'localstoragesaved') {
+				window.alert ('Local storage saved. Size {} characters.'.format (response ['size']));
+			}
+			else if (kind == 'synclocalstorage') {
+				var obj = response ['data'];
+				var i = 0;
+				for (var key in obj) {
+					localStorage.setItem (key, obj [key]);
+					i++;
+				}
+				window.alert ('Setting {} key(s) in localstorage done. Press ok to reload.'.format (i));
+				document.location.reload ();
+			}
+		}
+		catch (__except0__) {
+			print ('there was a problem processing config siores');
+		}
+	});},
 	get resize () {return __get__ (this, function (self, width, height) {
 		self.configsplitpane.resize (width, height);
 	});},
 	get build () {return __get__ (this, function (self) {
 		self.configsplitpane = SplitPane (dict ({'controlheight': 50}));
 		self.configdiv = Div ('largesheet');
-		self.configsplitpane.controlpanel.a (Button ('Serialize', self.serializeconfig).ac ('controlbutton'));
+		self.configsplitpane.controlpanel.a ([Button ('Serialize config', self.serializeconfig).ac ('controlbutton'), Button ('Serialize localstorage', self.serializelocalstorage).ac ('controlbutton'), Button ('Sync localstorage', self.synclocalstorage).ac ('controlbutton')]);
 		self.configsplitpane.controlpanel.ac ('subcontrolpanel');
 		self.configschema = Schema (self.schemaconfig);
 		self.configdiv.a (self.configschema);
