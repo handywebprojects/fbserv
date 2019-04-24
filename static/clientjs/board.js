32dabfc2f7a9aaf11958e9804163d1d71fbcae05
handywebprojects <handywebprojects@gmail.com>
2019-04-24 18:10:17 +0200

    Create screenshot of analysis info bin

--- static/clientjs/board.js
+++ static/clientjs/board.js
@@ -1,4 +1,4 @@
-// Transcrypt'ed from Python, 2019-04-20 22:21:12
+// Transcrypt'ed from Python, 2019-04-24 18:05:14
 import {AssertionError, AttributeError, BaseException, DeprecationWarning, Exception, IndexError, IterableError, KeyError, NotImplementedError, RuntimeWarning, StopIteration, UserWarning, ValueError, Warning, __JsIterator__, __PyIterator__, __Terminal__, __add__, __and__, __call__, __class__, __envir__, __eq__, __floordiv__, __ge__, __get__, __getcm__, __getitem__, __getslice__, __getsm__, __gt__, __i__, __iadd__, __iand__, __idiv__, __ijsmod__, __ilshift__, __imatmul__, __imod__, __imul__, __in__, __init__, __ior__, __ipow__, __irshift__, __isub__, __ixor__, __jsUsePyNext__, __jsmod__, __k__, __kwargtrans__, __le__, __lshift__, __lt__, __matmul__, __mergefields__, __mergekwargtrans__, __mod__, __mul__, __ne__, __neg__, __nest__, __or__, __pow__, __pragma__, __proxy__, __pyUseJsNext__, __rshift__, __setitem__, __setproperty__, __setslice__, __sort__, __specialattrib__, __sub__, __super__, __t__, __terminal__, __truediv__, __withblock__, __xor__, abs, all, any, assert, bool, bytearray, bytes, callable, chr, copy, deepcopy, delattr, dict, dir, divmod, enumerate, filter, float, getattr, hasattr, input, int, isinstance, issubclass, len, list, map, max, min, object, ord, pow, print, property, py_TypeError, py_iter, py_metatype, py_next, py_reversed, py_typeof, range, repr, round, set, setattr, sorted, str, sum, tuple, zip} from './org.transcrypt.__runtime__.js';
 import {LICH_API_GAMES_EXPORT, getconn, lichapiget} from './connection.js';
 import {Log, LogItem} from './widgets.js';
@@ -1308,6 +1308,14 @@ export var Board =  __class__ ('Board', [e], {
 			self.parsepgn (text);
 		}
 	});},
+	get screenshot () {return __get__ (this, function (self) {
+		self.chartdiv.x ();
+		html2canvas (self.analysisinfodiv.e).then ((function __lambda__ (canvas) {
+			return self.chartdiv.e.appendChild (canvas);
+		}), (function __lambda__ (err) {
+			return print (err);
+		}));
+	});},
 	get __init__ () {return __get__ (this, function (self, args) {
 		__super__ (Board, '__init__') (self, 'div');
 		self.gamei = 0;
@@ -1399,6 +1407,7 @@ export var Board =  __class__ ('Board', [e], {
 		self.autoanalysisbutton = Button ('A', self.autoanalysisclicked);
 		self.analysiscontrolpanelbottom.a (self.autoanalysisbutton);
 		self.copylinkbutton = Button ('C', self.copylink);
+		self.copylinkbutton = Button ('S', self.screenshot);
 		self.analysiscontrolpanelbottom.a (self.copylinkbutton);
 		self.lichessanalysisbutton = Button ('L', self.lichessanalysisclicked);
 		self.analysiscontrolpanelbottom.a (self.lichessanalysisbutton);
