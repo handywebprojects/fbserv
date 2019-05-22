0a6b05645bf05bfa30ed5c0bb2ad5e4d5669a595
handywebprojects <handywebprojects@gmail.com>
2019-05-22 08:10:51 +0200

    Copy PGN bin

--- static/clientjs/board.js
+++ static/clientjs/board.js
@@ -1,4 +1,4 @@
-// Transcrypt'ed from Python, 2019-04-24 18:05:14
+// Transcrypt'ed from Python, 2019-05-22 08:08:57
 import {AssertionError, AttributeError, BaseException, DeprecationWarning, Exception, IndexError, IterableError, KeyError, NotImplementedError, RuntimeWarning, StopIteration, UserWarning, ValueError, Warning, __JsIterator__, __PyIterator__, __Terminal__, __add__, __and__, __call__, __class__, __envir__, __eq__, __floordiv__, __ge__, __get__, __getcm__, __getitem__, __getslice__, __getsm__, __gt__, __i__, __iadd__, __iand__, __idiv__, __ijsmod__, __ilshift__, __imatmul__, __imod__, __imul__, __in__, __init__, __ior__, __ipow__, __irshift__, __isub__, __ixor__, __jsUsePyNext__, __jsmod__, __k__, __kwargtrans__, __le__, __lshift__, __lt__, __matmul__, __mergefields__, __mergekwargtrans__, __mod__, __mul__, __ne__, __neg__, __nest__, __or__, __pow__, __pragma__, __proxy__, __pyUseJsNext__, __rshift__, __setitem__, __setproperty__, __setslice__, __sort__, __specialattrib__, __sub__, __super__, __t__, __terminal__, __truediv__, __withblock__, __xor__, abs, all, any, assert, bool, bytearray, bytes, callable, chr, copy, deepcopy, delattr, dict, dir, divmod, enumerate, filter, float, getattr, hasattr, input, int, isinstance, issubclass, len, list, map, max, min, object, ord, pow, print, property, py_TypeError, py_iter, py_metatype, py_next, py_reversed, py_typeof, range, repr, round, set, setattr, sorted, str, sum, tuple, zip} from './org.transcrypt.__runtime__.js';
 import {LICH_API_GAMES_EXPORT, getconn, lichapiget} from './connection.js';
 import {Log, LogItem} from './widgets.js';
@@ -1316,6 +1316,11 @@ export var Board =  __class__ ('Board', [e], {
 			return print (err);
 		}));
 	});},
+	get copypgn () {return __get__ (this, function (self) {
+		self.tabpane.selectbykey ('pgn');
+		self.pgntext.textarea.e.select ();
+		document.execCommand ('copy');
+	});},
 	get __init__ () {return __get__ (this, function (self, args) {
 		__super__ (Board, '__init__') (self, 'div');
 		self.gamei = 0;
@@ -1406,13 +1411,16 @@ export var Board =  __class__ ('Board', [e], {
 		self.autoanalysis = false;
 		self.autoanalysisbutton = Button ('A', self.autoanalysisclicked);
 		self.analysiscontrolpanelbottom.a (self.autoanalysisbutton);
-		self.copylinkbutton = Button ('C', self.copylink);
-		self.copylinkbutton = Button ('S', self.screenshot);
+		self.copylinkbutton = Button ('H', self.copylink);
 		self.analysiscontrolpanelbottom.a (self.copylinkbutton);
+		self.screenshotbutton = Button ('S', self.screenshot);
+		self.analysiscontrolpanelbottom.a (self.screenshotbutton);
 		self.lichessanalysisbutton = Button ('L', self.lichessanalysisclicked);
 		self.analysiscontrolpanelbottom.a (self.lichessanalysisbutton);
 		self.lichessgamebutton = Button ('G', self.lichessgameclicked);
 		self.analysiscontrolpanelbottom.a (self.lichessgamebutton);
+		self.copypgnbutton = Button ('C', self.copypgn);
+		self.analysiscontrolpanelbottom.a (self.copypgnbutton);
 		var mopts = [];
 		for (var i = 1; i < 501; i++) {
 			mopts.append ([str (i), 'MultiPV {}'.format (i)]);
