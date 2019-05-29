605fbbbdb3c35416a1d8cf028fe1a4370b6f5fdc
handywebprojects <handywebprojects@gmail.com>
2019-05-29 07:46:05 +0200

    Copy and paste fen bin

--- static/clientjs/board.js
+++ static/clientjs/board.js
@@ -1,4 +1,4 @@
-// Transcrypt'ed from Python, 2019-05-22 08:08:57
+// Transcrypt'ed from Python, 2019-05-29 07:44:32
 import {AssertionError, AttributeError, BaseException, DeprecationWarning, Exception, IndexError, IterableError, KeyError, NotImplementedError, RuntimeWarning, StopIteration, UserWarning, ValueError, Warning, __JsIterator__, __PyIterator__, __Terminal__, __add__, __and__, __call__, __class__, __envir__, __eq__, __floordiv__, __ge__, __get__, __getcm__, __getitem__, __getslice__, __getsm__, __gt__, __i__, __iadd__, __iand__, __idiv__, __ijsmod__, __ilshift__, __imatmul__, __imod__, __imul__, __in__, __init__, __ior__, __ipow__, __irshift__, __isub__, __ixor__, __jsUsePyNext__, __jsmod__, __k__, __kwargtrans__, __le__, __lshift__, __lt__, __matmul__, __mergefields__, __mergekwargtrans__, __mod__, __mul__, __ne__, __neg__, __nest__, __or__, __pow__, __pragma__, __proxy__, __pyUseJsNext__, __rshift__, __setitem__, __setproperty__, __setslice__, __sort__, __specialattrib__, __sub__, __super__, __t__, __terminal__, __truediv__, __withblock__, __xor__, abs, all, any, assert, bool, bytearray, bytes, callable, chr, copy, deepcopy, delattr, dict, dir, divmod, enumerate, filter, float, getattr, hasattr, input, int, isinstance, issubclass, len, list, map, max, min, object, ord, pow, print, property, py_TypeError, py_iter, py_metatype, py_next, py_reversed, py_typeof, range, repr, round, set, setattr, sorted, str, sum, tuple, zip} from './org.transcrypt.__runtime__.js';
 import {LICH_API_GAMES_EXPORT, getconn, lichapiget} from './connection.js';
 import {Log, LogItem} from './widgets.js';
@@ -1321,6 +1321,17 @@ export var Board =  __class__ ('Board', [e], {
 		self.pgntext.textarea.e.select ();
 		document.execCommand ('copy');
 	});},
+	get copyfen () {return __get__ (this, function (self) {
+		self.basicboard.fentext.e.select ();
+		document.execCommand ('copy');
+	});},
+	get autopastefen () {return __get__ (this, function (self) {
+		navigator.clipboard.readText ().then ((function __lambda__ (clipText) {
+			return self.fentextchangedcallback (clipText);
+		}), (function __lambda__ (err) {
+			return print (err);
+		}));
+	});},
 	get __init__ () {return __get__ (this, function (self, args) {
 		__super__ (Board, '__init__') (self, 'div');
 		self.gamei = 0;
@@ -1400,6 +1411,8 @@ export var Board =  __class__ ('Board', [e], {
 		self.analysiscontrolpaneltop.a (Button ('Store >', self.storeforward).ac ('analysismake'));
 		self.analysiscontrolpaneltop.a (Button ('Store Make', self.storemake).ac ('analysismake'));
 		self.analysiscontrolpaneltop.a (Button ('Store Stop', self.stopandstoreanalysis).ac ('analysisstop'));
+		self.analysiscontrolpaneltop.a (Button ('CF', self.copyfen));
+		self.analysiscontrolpaneltop.a (Button ('PF', self.autopastefen));
 		self.analysiscontrolpanelbottom = Div ().ac ('bigboardanalysiscontrolpanel');
 		self.analysiscontrolpanelbottom.a (Button ('#', self.getstoredanalysisinfo).ac ('analysisanalyze'));
 		self.analysiscontrolpanelbottom.a (Button ('Analyze', self.analyzecallbackfactory ()).ac ('analysisanalyze'));
